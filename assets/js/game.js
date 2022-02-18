d90bbd67a2eff9f7ec7e9284c69de5c61df389fd
Deena Smith <deenasmith1630@gmail.com>
2022-02-18 17:13:48 -0600

    randomize health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,10 +8,18 @@ var playerMoney = 10;
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]; 
 var enemyHealth = 30;
 var enemyAttack = 12;
-// Math.floor(Math.random() * (10 - 1 + 1) + 1);
+
 
 console.log("EA",enemyAttack);
 console.log("EH",enemyHealth);
+
+// Function to generate random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 
     // Alert that players are starting
 var fight = function(enemyName) {
@@ -23,8 +31,11 @@ var fight = function(enemyName) {
 
       // if player choses to fight, then fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
-      // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+
+      // generate random damage value based on player attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
       );
@@ -37,8 +48,10 @@ var fight = function(enemyName) {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
     
-      // remove player's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      // generate random damage value based on enemy attack power
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      
+      playerHealth = Math.max(0, playerHealth - enemyAttack);
       console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
       );
@@ -60,7 +73,7 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
@@ -74,6 +87,7 @@ var fight = function(enemyName) {
 }
 
 
+// Begin Game function
 var startGame = function() {
 
   // reset player stats
@@ -87,7 +101,7 @@ var startGame = function() {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       fight(pickedEnemyName);
 
@@ -112,6 +126,7 @@ var startGame = function() {
   endGame();
 };
 
+
 // function to end entire game
 
 var endGame = function() {
@@ -133,6 +148,7 @@ var endGame = function() {
   }
   
 }
+
 
 // SHOP function
 
