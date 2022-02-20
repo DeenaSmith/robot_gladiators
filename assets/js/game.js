028643bfdf7d298f18071851ea1d1f926d27fad6
Deena Smith <deenasmith1630@gmail.com>
2022-02-20 10:24:45 -0600

    player name bug repaired

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,23 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
+
 // Player Info Object
 var welcomeWindow = window.alert("Welcome to Robot Gladiators!");
 
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
