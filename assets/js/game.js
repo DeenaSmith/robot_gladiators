3d53f32c39b71a020e0d8aab83dd1b9360c2cce5
Deena Smith <deenasmith1630@gmail.com>
2022-02-20 12:58:59 -0600

    high score feature added

--- assets/js/game.js
+++ assets/js/game.js
@@ -193,11 +193,22 @@ var startGame = function() {
 // function to end entire game
 
 var endGame = function() {
-  // if player lives, player wins
-  if (playerInfo.health > 0) {
-    window.alert("Excellent work, you have survived. You now have a score of " + playerInfo.money + ".");
-  } else {
-    window.alert("You have lost your battle. The fight has ended.");
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   // ask player if they would like to play again
